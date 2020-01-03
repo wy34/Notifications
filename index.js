36a26968cf33334b364ed472fd44fe96a448a7c8
William Yeung <williamyeung1141998@gmail.com>
2020-01-02 23:45:01 -0500

    Implemented select/deselect feature

--- index.js
+++ index.js
@@ -17,3 +17,24 @@ let albums = document.querySelectorAll(".album");
 let itemsTotal = document.querySelector(".items-total");
 let savedBtn = document.querySelector(".button");
 
+let albumSelected = [];
+let i = 0;
+while (i < albums.length) {
+    albums[i].addEventListener("click", function() {
+        let albumTitle = this.querySelector(".title").textContent;
+        if (!this.getAttribute("class").includes("selected")) {
+            this.classList.add("selected")
+            albumSelected.push(albumTitle);
+            console.log(albumSelected)
+        } else {
+            this.classList.remove("selected");
+            // let removeIndex = albumSelected.indexOf(albumTitle)
+            // albumSelected.splice(removeIndex, 1);
+            albumSelected = albumSelected.filter(album => {
+                return album !== albumTitle;
+            })
+            console.log(albumSelected)
+        }
+    });
+    i++;
+}
