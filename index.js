ba38638bf65484a8bea5fbdeddf814f46ac916b1
William Yeung <williamyeung1141998@gmail.com>
2020-01-02 22:34:44 -0500

    Added pseudocode explaining the steps

---
+++ index.js
@@ -0,0 +1,9 @@
+// Click album 
+
+// Show user he selected album so we add a class selected to the album div
+
+// Create an array and then add the album's title we selected to that array
+
+// Find out the total of the albums selected
+
+// Save button is clicked then when its clicked show notification with saved items total
